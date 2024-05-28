046f6f0087f320227a4abfdbec8454d5c15619b1
고서온 <ice1github@goseoon-ui-MacBookAir.local>
2024-05-28 11:03:31 +0900

    [perf] python files for loading sounds

---
+++ sounds.py
@@ -0,0 +1,31 @@
+import pygame
+
+pygame.init()
+
+MAIN_MUSIC_1 = pygame.mixer.Sound('sounds/013. Ambient Music1.mp3')
+MAIN_MUSIC_2 = pygame.mixer.Sound('sounds/014. Ambient Music2.mp3')
+LOBBY_MUSIC_3 = pygame.mixer.Sound('sounds/088. Breaker Box Hum.mp3')
+
+INTRO_MUSIC = pygame.mixer.Sound('sounds/427. Intro Company Speech.mp3')
+
+DOOR_OPENING = pygame.mixer.Sound('sounds/389. Hangar Door Opening.mp3')
+
+METAL_DOOR_SHUT = pygame.mixer.Sound('sounds/514. Metal Door Shut1 1.mp3')
+NOISE = pygame.mixer.Sound('sounds/673. Ship Thruster Ambiance.mp3')
+
+METAL_WALK_1 = pygame.mixer.Sound('sounds/523. Metal Walk.mp3')
+METAL_WALK_2 = pygame.mixer.Sound('sounds/524. Metal Walk2.mp3')
+METAL_WALK_3 = pygame.mixer.Sound('sounds/525. Metal Walk3.mp3')
+METAL_WALK_4 = pygame.mixer.Sound('sounds/526. Metal Walk4.mp3')
+
+GRAB_SHOVEL = pygame.mixer.Sound('sounds/367. Grab Shovel.mp3')
+GRAB_KEY = pygame.mixer.Sound('sounds/363. Grab Key.mp3')
+GRAB_BOTTLE = pygame.mixer.Sound('sounds/359. Grab Bottle.mp3')
+GRAB_FLASHLIGHT = pygame.mixer.Sound('sounds/362. Grab Flashlight.mp3')
+
+DROP_SHOVEL = pygame.mixer.Sound('sounds/278. Drop Shovel.mp3')
+DROP_KEY = pygame.mixer.Sound('sounds/267. Drop Key.mp3')
+DROP_BOTTLE = pygame.mixer.Sound('sounds/258. Drop Bottle Single.mp3')
+DROP_FLASHLIGHT = pygame.mixer.Sound('sounds/262. Drop Flashlight.mp3')
+
+FLASHLIGHT_CLICK = pygame.mixer.Sound('sounds/317. Flashlight Click.mp3')
