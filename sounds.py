3046652142361f39e65786771edbee45e22a6af1
고서온 <ice1github@goseoon-ui-MacBookAir.local>
2024-06-03 19:45:43 +0900

    [style] added sounds

--- sounds.py
+++ sounds.py
@@ -7,11 +7,16 @@ MAIN_MUSIC_2 = pygame.mixer.Sound('sounds/014. Ambient Music2.mp3')
 LOBBY_MUSIC_3 = pygame.mixer.Sound('sounds/088. Breaker Box Hum.mp3')
 
 INTRO_MUSIC = pygame.mixer.Sound('sounds/427. Intro Company Speech.mp3')
+MENU_MUSIC = pygame.mixer.Sound('sounds/512. Menu1.mp3');
+
+DELIVERY_MUSIC = pygame.mixer.Sound('sounds/Voicy_Lethal Company Ice Cream Truck Theme.mp3');
 
 DOOR_OPENING = pygame.mixer.Sound('sounds/389. Hangar Door Opening.mp3')
 
 METAL_DOOR_SHUT = pygame.mixer.Sound('sounds/514. Metal Door Shut1 1.mp3')
+METAL_DOOR_CLOSE = pygame.mixer.Sound('sounds/517. Metal Hatch Close.mp3')
 NOISE = pygame.mixer.Sound('sounds/673. Ship Thruster Ambiance.mp3')
+NOISE_2 = pygame.mixer.Sound('sounds/399. High Wind.mp3')
 
 METAL_WALK_1 = pygame.mixer.Sound('sounds/523. Metal Walk.mp3')
 METAL_WALK_2 = pygame.mixer.Sound('sounds/524. Metal Walk2.mp3')
@@ -22,6 +27,7 @@ GRAB_SHOVEL = pygame.mixer.Sound('sounds/367. Grab Shovel.mp3')
 GRAB_KEY = pygame.mixer.Sound('sounds/363. Grab Key.mp3')
 GRAB_BOTTLE = pygame.mixer.Sound('sounds/359. Grab Bottle.mp3')
 GRAB_FLASHLIGHT = pygame.mixer.Sound('sounds/362. Grab Flashlight.mp3')
+GRAB_DEFAULT_ITEM = pygame.mixer.Sound('sounds/360. Grab Cardboard Box.mp3')
 
 DROP_SHOVEL = pygame.mixer.Sound('sounds/278. Drop Shovel.mp3')
 DROP_KEY = pygame.mixer.Sound('sounds/267. Drop Key.mp3')
@@ -29,3 +35,8 @@ DROP_BOTTLE = pygame.mixer.Sound('sounds/258. Drop Bottle Single.mp3')
 DROP_FLASHLIGHT = pygame.mixer.Sound('sounds/262. Drop Flashlight.mp3')
 
 FLASHLIGHT_CLICK = pygame.mixer.Sound('sounds/317. Flashlight Click.mp3')
+
+BRACKEN_ATTACK = pygame.mixer.Sound('sounds/Voicy_bracken neck snap (lethal company).mp3')
+JESTER_DOING = pygame.mixer.Sound('sounds/Voicy_Jester.mp3')
+COILHEAD_ATTACK_1 = pygame.mixer.Sound('sounds/Voicy_Spring Wobble 1.mp3')
+COILHEAD_ATTACK_2 = pygame.mixer.Sound('sounds/Voicy_Spring Wobble 2.mp3')
